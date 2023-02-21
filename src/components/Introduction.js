63f393577fb4652301a9a04ac35a11dc4f54810b
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-21 15:09:32 -0600

    Introduction almost done

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,18 +1,18 @@
-import { Box, Container, Flex, Heading } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, Stack } from "@chakra-ui/react"
 
 export const Introduction = () => {
   return (
     <Box> 
       <Container as='Flex'size={{lg: 'lg'}} alignItems='left'>
-        <Heading as='h1' size='2xl' noOfLines={1}>
+        <Heading as='h1' size='2xl' noOfLines={1} marginBottom='1.5rem'>
           Hello World!
         </Heading>
-        <ul>
+        <Stack as='ul' listStyleType='none'>
           <li>I´m Emiliano Carrizales, a 22 years-old Frontend Developer</li>
           <li>Self taught programmer, an exercise addict, and a life enyojer</li>
           <li>A begginer in this Web Development World, learning to use Next.js</li>
-          <li>Living in this World</li>
-        </ul>
+          <li>Always living in my mind</li>
+        </Stack>
       </Container>
     </Box>
   )
