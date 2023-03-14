b74643bc220af438918d2bc67076995a55d9fc59
EmilianoCBE <e.carrizales1305@gmail.com>
2023-03-13 20:53:52 -0600

    Dark/Light mode button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
 import { Container, Box, Flex, Button } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useColorMode } from '@chakra-ui/react'
 
 export const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <Box bg='white' w='100%' p={2} marginBottom='1rem'>
-      <Container as={Flex} maxWidth='1110px' color='black' justifyContent='space-between' fontSize="md" fontWeight='bold' alignItems='center' size={{lg: 'lg'}}>
-          <Flex gap='1.5rem'>
+    <Box w='100%' p={2} marginBottom='1rem'>
+      <Container as={Flex} maxWidth='1110px' justifyContent='space-between' fontSize="md" fontWeight='bold' alignItems='center' size={{lg: 'lg'}}>
+          <Flex gap='1.5rem' alignItems='center'>
               <Flex><Link href='#'>Emiliano</Link></Flex>
               {/* <Flex><Button><Image src='/public/moon.svg' alt='moon' width='20' height='20' /></Button></Flex> */}
+              <Flex>
+                <Button onClick={toggleColorMode}>
+                  {/* {colorMode === 'light' ? <Image src='/moon.svg' alt='moon' width='20' height='20' /> : <Image src='/sun.svg' alt='moon' width='20' height='20' />} */}
+                  {colorMode === 'light' ? 'Dark mode' : 'Light mode'}
+                </Button>
+              </Flex>
           </Flex>
 
           <Flex listStyleType="none" gap="1rem" alignItems='center'>
             <Link href="https://www.linkedin.com/in/emiliano-carrizales">
-              <Image src="/icon-in.svg" alt="" width={32} height="32"/>
+              <Image src="/icon-in.svg" alt="" width={32} height="32" color='white'/>
             </Link>
             <Link href="https://github.com/EmilianoCBE">
               <Image src="/icon-github.svg" alt="" width={32} height="32"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,12 @@ import { Header } from '@/components/Header'
 import { Introduction } from '@/components/Introduction'
 import { Button, Container } from '@chakra-ui/react'
 import { Description } from '@/components/Description'
-import { useColorMode } from '@chakra-ui/react'
+
 // import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode()
-
   return (
     <>
       <Head>
@@ -29,9 +27,7 @@ export default function Home() {
           <Description />
         </Container>
       </main>
-      <Button onClick={toggleColorMode}>
-          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-        </Button>
+      
     </>
   )
 }
