ef6c7107b30354c05d1cd216f5aed883f2d0a5b1
EmilianoCBE <e.carrizales1305@gmail.com>
2023-03-14 14:51:53 -0600

    Mobile changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Container, Box, Flex, Button } from '@chakra-ui/react'
+import { Container, Box, Flex, Button, Show, Hide } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useColorMode } from '@chakra-ui/react'
@@ -9,51 +9,37 @@ export const Header = () => {
   return (
     <Box w='100%' p={2} marginBottom='1rem'>
       <Container as={Flex} maxWidth='1110px' justifyContent='space-between' fontSize="md" fontWeight='bold' alignItems='center' size={{lg: 'lg'}}>
-          <Flex gap='1.5rem' alignItems='center'>
-              <Flex><Link href='#'>Emiliano</Link></Flex>
+          <Flex gap='1.5rem' alignItems='center' width={{base: '100%', md: 'initial'}} justifyContent='space-between'>
+              <Flex>
+                <Link href='#'>Emiliano</Link>
+              </Flex>
               {/* <Flex><Button><Image src='/public/moon.svg' alt='moon' width='20' height='20' /></Button></Flex> */}
               <Flex>
-                <Button onClick={toggleColorMode}>
-                  {/* {colorMode === 'light' ? <Image src='/moon.svg' alt='moon' width='20' height='20' /> : <Image src='/sun.svg' alt='moon' width='20' height='20' />} */}
-                  {colorMode === 'light' ? 'Dark mode' : 'Light mode'}
-                </Button>
+                  <Button onClick={toggleColorMode}>
+                    {/* {colorMode === 'light' ? <Image src='/moon.svg' alt='moon' width='20' height='20' /> : <Image src='/sun.svg' alt='moon' width='20' height='20' />} */}
+                    {colorMode === 'light' ? 'Dark mode' : 'Light mode'}
+                  </Button>
               </Flex>
           </Flex>
 
-          <Flex listStyleType="none" gap="1rem" alignItems='center'>
-            <Link href="https://www.linkedin.com/in/emiliano-carrizales">
-              {colorMode === 'light' ? 
-                <Image src="/icon-in-light.svg" alt="" width="32" height="32"/>
-                : <Image src="/icon-in-dark.svg" alt="" width="32" height="32"/>
-              }
-            </Link>
-            <Link href="https://github.com/EmilianoCBE">
-              {colorMode === 'light' ? 
-                <Image src="/icon-github-light.svg" alt="" width="32" height="32"/>
-                : <Image src="/icon-github-dark.svg" alt="" width="32" height="32"/>
-              }
-              {/* <Image src="/icon-github-light.svg" alt="" width={32} height="32"/> */}
-            </Link>
-            <Link href="https://emilianocbe.github.io">Portfolio</Link>
-          </Flex>
-
-          {/* <Flex gap="1rem">
+          <Hide below='sm'>
+            <Flex listStyleType="none" gap="1rem" alignItems='center'>
+              <Link href="https://www.linkedin.com/in/emiliano-carrizales">
+                {colorMode === 'light' ? 
+                  <Image src="/icon-in-light.svg" alt="" width="32" height="32"/>
+                  : <Image src="/icon-in-dark.svg" alt="" width="32" height="32"/>
+                }
+              </Link>
               <Link href="https://github.com/EmilianoCBE">
-                <Image src="/icon-github.svg" alt="" width={24} height="24"/>
+                {colorMode === 'light' ? 
+                  <Image src="/icon-github-light.svg" alt="" width="32" height="32"/>
+                  : <Image src="/icon-github-dark.svg" alt="" width="32" height="32"/>
+                }
+                {/* <Image src="/icon-github-light.svg" alt="" width={32} height="32"/> */}
               </Link>
-              
-              <Link href="#">
-                <Image src="/icon-twiter.svg" alt="" width={24} height="24"/>
-              </Link>
-              
-              <Link href="#">
-                <Image src="/icon-instagram.svg" alt="" width={24} height="24"/>
-              </Link>
-              
-              <Link href="#">
-                <Image src="/icon-pinterest.svg" alt="" width={24} height="24"/>
-              </Link>
-          </Flex>   */}
+              <Link href="https://emilianocbe.github.io">Portfolio</Link>
+            </Flex>
+          </Hide>
         </Container>          
     </Box>
   )
